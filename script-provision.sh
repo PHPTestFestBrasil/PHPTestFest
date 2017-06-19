13789e9779f8899c04830838cae9aa0e9e8bf7ef
Rodrigo Prado <royopa@gmail.com>
2017-06-18 22:44:36 -0300

    added readine

--- script-provision.sh
+++ script-provision.sh
@@ -50,7 +50,7 @@ echo "********************************"
 sudo -u vagrant git clone https://github.com/php/php-src /home/vagrant/php-src
 cd /home/vagrant/php-src
 sudo -u vagrant ./buildconf
-sudo -u vagrant ./configure --enable-gcov --enable-debug --enable-sigchild --enable-libgcc --with-openssl --with-kerberos --with-pcre-regex --enable-bcmath --with-bz2 --enable-calendar --with-curl --with-enchant --enable-exif --enable-ftp --with-gd --enable-gd-jis-conv --with-gettext --with-mhash --with-kerberos --with-imap-ssl --enable-intl --enable-mbstring --with-libmbfl --with-onig --with-pspell --with-recode --with-mm --enable-shmop --with-snmp --enable-soap --enable-sockets --enable-sysvsem --enable-wddx --with-xmlrpc --with-xsl --enable-zip --with-zlib
+sudo -u vagrant ./configure --enable-gcov --enable-debug --enable-sigchild --enable-libgcc --with-openssl --with-kerberos --with-pcre-regex --enable-bcmath --with-bz2 --enable-calendar --with-curl --with-enchant --enable-exif --enable-ftp --with-gd --enable-gd-jis-conv --with-gettext --with-mhash --with-kerberos --with-imap-ssl --enable-intl --enable-mbstring --with-libmbfl --with-onig --with-pspell --with-recode --with-mm --enable-shmop --with-snmp --enable-soap --enable-sockets --enable-sysvsem --enable-wddx --with-xmlrpc --with-xsl --enable-zip --with-zlib --with-readline
 sudo -u vagrant make
 
 echo "**********************************************************"
